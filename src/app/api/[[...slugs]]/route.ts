61852f9e1f33fd5420e1e04e4a18d7423467e674
Sujoy Manna <62413879+sujoy-2811@users.noreply.github.com>
2025-12-10 02:12:26 +0530

    feat: add  elysia and tanstack react query libraries

---
+++ src/app/api/[[...slugs]]/route.ts
@@ -0,0 +1,10 @@
+import { Elysia, t } from "elysia";
+
+const app = new Elysia({ prefix: "/api" }).get("/user", {
+  user: { name: "Sujoy" },
+});
+
+export const GET = app.fetch;
+export const POST = app.fetch;
+
+export type App = typeof app;

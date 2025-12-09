22a410127c1f47269f092b6d8e52f129021d5b1f
Sujoy Manna <62413879+sujoy-2811@users.noreply.github.com>
2025-12-10 02:51:18 +0530

    feat: test intial endpoint

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,8 +1,15 @@
 import { Elysia, t } from "elysia";
+import { nanoid } from "nanoid";
 
-const app = new Elysia({ prefix: "/api" }).get("/user", {
-  user: { name: "Sujoy" },
+const room = new Elysia({ prefix: "/room" }).post("/create", () => {
+  console.log("Create a new room. ");
 });
+
+const app = new Elysia({ prefix: "/api" })
+  .get("/user", {
+    user: { name: "Sujoy" },
+  })
+  .use(room);
 
 export const GET = app.fetch;
 export const POST = app.fetch;

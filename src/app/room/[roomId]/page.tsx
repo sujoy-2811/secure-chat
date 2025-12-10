b5c9815ba0867e9fc8f3e5a569756e4dff128362
Sujoy Manna <62413879+sujoy-2811@users.noreply.github.com>
2025-12-11 04:52:47 +0530

    feat:  add input message box and send button

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function formatTimeRemaining(seconds: number) {
   const mins = Math.floor(seconds);
@@ -17,6 +17,9 @@ const Page = () => {
 
   const [copyStatus, setCopyStatus] = useState("Copy");
   const [timeRaining, setTimeRemaining] = useState<number | null>(null);
+
+  const [input, setInput] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const copyLink = () => {
     const url = window.location.href;
@@ -66,6 +69,35 @@ const Page = () => {
           Destroy Now
         </button>
       </header>
+      <div className="flex-1 overflow-y-auto p-4 space-y-4 -----"></div>
+
+      <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
+        <div className="flex gap-4">
+          <div className="flex-1 relative group">
+            <span className="absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse">
+              {">"}
+            </span>
+            <input
+              autoFocus
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key == "Enter" && input.trim()) {
+                  // future code
+                  setInput("");
+                  inputRef.current?.focus();
+                }
+              }}
+              placeholder="Type message..."
+              type="text"
+              className="w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm"
+            />
+          </div>
+          <button className=" bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-pointer">
+            SEND
+          </button>
+        </div>
+      </div>
     </main>
   );
 };

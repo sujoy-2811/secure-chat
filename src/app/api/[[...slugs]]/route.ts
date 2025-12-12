ef942eaa7faba9815c7673555386b33e0a701a79
Sujoy Manna <62413879+sujoy-2811@users.noreply.github.com>
2025-12-12 09:39:06 +0530

    feat: add auth middleware and integrate zod for message validation

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,9 @@
 import { redis } from "@/lib/redis";
+import { authMiddleware } from "./auth";
 import { Elysia, t } from "elysia";
 import { nanoid } from "nanoid";
+import z from "zod";
+import { Bodoni_Moda } from "next/font/google";
 
 const ROOM_TTL_SECONDS = 60 * 10;
 const room = new Elysia({ prefix: "/room" }).post("/create", async () => {
@@ -18,11 +21,23 @@ const room = new Elysia({ prefix: "/room" }).post("/create", async () => {
   return { roomId };
 });
 
-const app = new Elysia({ prefix: "/api" })
-  .get("/user", {
-    user: { name: "Sujoy" },
-  })
-  .use(room);
+const messages = new Elysia({ prefix: "/message" }).use(authMiddleware).post(
+  "/",
+  async ({ body, auth }) => {
+    const { roomId } = auth;
+    const { sender, text } = body;
+    const roomExists = await redis.exists(`meta:${roomId}`);
+  },
+  {
+    query: z.object({ roomId: z.string() }),
+    body: z.object({
+      sender: z.string().max(100),
+      text: z.string().max(100),
+    }),
+  }
+);
+
+const app = new Elysia({ prefix: "/api" }).use(room).use(messages);
 
 export const GET = app.fetch;
 export const POST = app.fetch;
